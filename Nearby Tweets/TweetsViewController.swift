f3a36b1fc29d73081c6af73a12e3edcac1a7062f
r3dcrosse <mrdavidwayman@gmail.com>
2015-12-03 20:37:35 -0800

    Added Fabric framework
    Added Fabric framework to handle the twitter api.

---
+++ Nearby Tweets/TweetsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nearby Tweets
+//
+//  Created by David Wayman on 12/2/15.
+//  Copyright © 2015 David Wayman. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
